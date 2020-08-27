7077e679b302cf7b9d130d89d47c381ef3f77bf6
devomaa <lmao@distruzione.org>
2020-08-27 16:57:04 +0300

    Update daemon.lua

--- daemon.lua
+++ daemon.lua
@@ -1,8 +1,10 @@
 --I have no idea, but it just works.
 while true do
-    local evs = {os.pullEvent("soqet_message")}
+    parallel.waitForAll(function() local evs = {os.pullEvent("soqet_message")}
     if evs then
         os.queueEvent("modem_message", "soqet", evs[2], (evs[4]["replyChannel"] or evs[2]), evs[3], (evs[4]["dist"] or 1))
-        print("Received \"", evs[3], "\" in channel ", evs[2], "(reply channel: ", (evs[4]["replyChannel"] or evs[2]), ")")
-    end
+    end end, function()
+		local event, peripheral_name, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+		print("Received \"", message "\"on channel", channel, "at", distance, "blocks away. (reply channel: " replyChannel, ")")
+	end)
 end
